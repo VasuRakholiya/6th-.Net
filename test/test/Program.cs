2ae95e9fab30c35bece3dcbcc4be56f4981cf818
VasuRakholiya <vasurakholiya7877@gmail.com>
2022-11-28 15:34:21 +0530

    28-11-22

---
+++ test/test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter=1;
+            //while (counter <= 100)
+            //{
+            //    Console.Write("{0,5}",counter);
+            //    counter = counter + 1;
+            //}
+
+            do
+            {
+                Console.WriteLine("{0,5}",counter);
+                counter++;
+            }while(counter <=100);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ test2/test2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("print using do while");
+            int counter = 1;
+            bool keepgoing = true;
+
+            do
+            {
+                Console.Write("{0,5}", counter);
+                if(counter%100==0 && counter!=0)
+                {
+                    Console.WriteLine("\n keep counting..<y/n>");
+                    if(Console.ReadLine()!="y")
+                    {
+                        keepgoing = false;
+                    }
+                }
+                counter++;
+            } while (keepgoing);
+            Console.WriteLine("finish counting");
+            Console.ReadLine();
+        }YieldAwaitable
+
+    }
+}
