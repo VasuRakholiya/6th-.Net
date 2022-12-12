445355039719455a3947ad41e563464310b584d6
VasuRakholiya <vasurakholiya7877@gmail.com>
2022-12-12 15:31:33 +0530

    12-12-22

---
+++ static function/static function/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_function
+{
+    class abcd
+    {
+        public int count = 0;
+        public  static void printcount()
+        {
+            abcd obj=new abcd();
+            obj.count=obj.count+1;
+            //count = count + 1;
+           // Console.WriteLine("the value is {0}",count);
+        }
+    }
+    class Program
+    {
+        static static void Main(string[] args)
+        {
+            abcd a = new abcd();
+            abcd.printcount();
+            abcd.printcount();
+            //a.printcount();
+            //a.printcount();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ static variable/static variable/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace static_variable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class join
+        {
+            static int balance = 10000;
+            public void withdraw(int amt)
+            {
+                balance=balance-amt;
+                MessageBox.Show("the current balance is" + balance);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            join j= new join();
+            j.withdraw(Convert.ToInt32(textBox1.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            join j = new join();
+            j.withdraw(Convert.ToInt32(textBox1.Text));
+        }
+    }
+}
