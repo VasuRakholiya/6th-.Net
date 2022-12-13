95bda4fe198ccf8ee089257fa707dd85ce487680
VasuRakholiya <vasurakholiya7877@gmail.com>
2022-12-13 14:17:45 +0530

    13-12-22

---
+++ array/array/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            int i, j, sum = 0;
+            Console.WriteLine("\n reading array");
+            for(i=0;i<a.Length;i++)
+            {
+                Console.WriteLine("enter element[{0}]",i+1);
+                a[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\n printing the array");
+            for(i=0;i< a.Length;i++)
+            {
+                Console.WriteLine("{0,4}", a[i]);
+            }
+            Console.WriteLine("\n calculating sum of elements in array");
+            for(i=0;i< a.Length ;i++)
+            {
+                sum = sum + a[i];
+            }
+            Console.WriteLine("\n total sum of array is {0}", sum);
+            Console.WriteLine("\n displaying array using for each loop");
+            foreach(int ele in a)
+            {
+                Console.WriteLine("{0,4}", ele);
+            }
+            Console.WriteLine("\n enter the element to be searched in array");
+            int searchele=Convert.ToInt32(Console.ReadLine());
+            for(i=0;i<=a.Length;i++)
+            {
+                if (a[i]==searchele)
+                {
+                    Console.WriteLine("the element {0} is found at {1} position",searchele,i+1);
+                }
+            }
+            Console.WriteLine("\n sortint in array");
+            for (i = 0; i < a.Length; i++)
+            {
+                for(j=i+1;j<a.Length;j++)
+                {
+                    if (a[i] > a[j])
+                    {
+                        int temp = a[i];
+                        temp = a[j];
+                        a[j] = a[i];
+                        a[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\n array after sorting");
+            for (i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("{0,4}", a[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ error handling/error handling/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace error_handling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = Convert.ToInt32(textBox1.Text);
+                int b = Convert.ToInt32(textBox2.Text);
+                int c = a / b;
+                textBox3.Text = c.ToString();
+            }
+            catch(DivideByZeroException cc) {
+                MessageBox.Show("cant divide by zero" + cc.Message);
+            }
+            catch(FormatException cc)
+            {
+                MessageBox.Show("dont enter characters" + cc.Message);
+            }
+            catch(Exception cc)
+            {
+                MessageBox.Show("some genral error come" + cc.Message);
+            }
+            finally
+            {
+                MessageBox.Show("i am alive");
+            }
+        }
+        class Axisbank:ApplicationException
+        {
+            public Axisbank(string message):base(message)
+            {
+
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int age=Convert.ToInt32(textBox4.Text);
+                if(age<18)
+                {
+                    throw new Axisbank("abe below 18 can not open account");
+                }
+            }
+            catch(Axisbank cc)
+            {
+                MessageBox.Show(cc.Message);
+            }
+        }
+    }
+}
